3adcf8cd8773519961b0e918a738c71d05b1757b
Rizky Faisal <rizkyfaisal.career@gmail.com>
2026-03-21 01:20:24 +0700

    Add files via upload
    Pembuatan insialisasi variabel pada truct, dan pemanggilannya menggunakan array

---
+++ Struct.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>  
+#include <string.h>
+
+using namespace std;
+
+int main (){
+struct profil{
+    string nama,hobi,pendidikan;
+    int umur;
+};
+
+profil p[2] = {
+        {"Rizky Faisal", "Olahraga", "S1 Teknik Komputer", 20}, 
+        {"Angga Adi Pratama", "Olahraga", "S2 Ilmu Komunikasi", 21}     
+    };
+
+for (int i = 0;i<2;i++){
+    cout <<"\nProfil ke-"<< i+1 <<"\nnama = " << p[i].nama <<"\n"<<"Hobi = "<< p[i].hobi<<"\n"<<"Pendidikan = "<<p[i].pendidikan <<"\n"<<"Umur = "<< p[i].umur<<"\n";
+  
+}
+
+return 0;
+}
